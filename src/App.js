ebe303305f93abd8d74f9b5e10b330835e5f31b5
nalbandmohammad <nmohammadrafi11@gmail.com>
2020-04-04 13:11:12 +0530

    My Store First Commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Create Store
         </a>
       </header>
     </div>
