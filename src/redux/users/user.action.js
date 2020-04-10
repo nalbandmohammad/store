88a70069ac47d97893a64160cd5136748769a9c9
nalbandmohammad <nmohammadrafi11@gmail.com>
2020-04-10 12:46:45 +0530

    Updated shop data with new data, updated cart utils to group items in cart reducer

--- src/redux/users/user.action.js
+++ src/redux/users/user.action.js
@@ -2,6 +2,6 @@
 import { UserActionTypes } from './user.types'; 
 
 export const setCurrentUser = (user) => ({
-    type: 'SET_CURRENT_USER',
+    type: UserActionTypes.SET_CURRENT_USER,
     payload: user
 })
